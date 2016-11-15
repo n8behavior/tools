c2184c31deb59743633a5d22289c0faf9b2b2ba7
n8behavior <mike@thesandmans.com>
2016-11-15 15:56:08 -0500

    repo-labels now uses github include (see #15)

--- src/repo-labels.sh
+++ src/repo-labels.sh
@@ -1,57 +1,20 @@
 #!/bin/bash
 
 # common functions
-source @BASHLIBS@/common
-TOKEN_VAR='puterstructions.tools.token'
+source @BASHLIBS@/github
 
 # setup script options
-source @BASHLIBS@/shflags
 DEFINE_string name '' 'Name for the GitHub repository' 'n'
-DEFINE_string org 'puterstructions' 'Organization for the GitHub repository' 'o'
-DEFINE_string username "$(git config user.name)" 'GitHub user for authentication' 'u'
-DEFINE_string token "$(git config $TOKEN_VAR)" 'GitHub token for authentication' 't'
 DEFINE_boolean list false 'List all existing labels' 'l'
 DEFINE_boolean delete false 'Delete all existing labels' 'd'
 DEFINE_boolean add false 'Add standard labels' 'a'
-DEFINE_boolean version false 'Version of the tools package' 'v'
 FLAGS "$@" || log ERROR 'Failed to parse flags'
 eval set -- "${FLAGS_ARGV}"
-
-# exit if we have displayed help text
-[ "${FLAGS_help}" = "${FLAGS_TRUE}" ] && exit 0
-
-# exist after showing the version
-[ "${FLAGS_version}" = "${FLAGS_TRUE}" ] && 
-{
-    echo "PACKAGE_VERSION"
-    exit 0
-}
-
-# ensure jq is installed
-JQ=$(which jq) || 
-{
-    log ERROR "Could not find jq. Is it installed?"
-    exit 1
-}
 
 # ensure we have a repo name
 [ "$FLAGS_name" ] || 
 {
     log ERROR "You must provide a repository name"
-    exit 1
-}
-
-# ensure we have github user
-[ "$FLAGS_username" ] || 
-{
-    log ERROR "Could not find username.\nRun 'git config --global github.user <username>'"
-    exit 1
-}
-
-# ensure we have a token
-[ "$FLAGS_token" ] || 
-{
-    log ERROR "Could not find token.\nRun 'git config --global $TOKEN_VAR <token>'"
     exit 1
 }
 
