71a4b56bfda108da70df0f9696228bdc0416bed5
n8behavior <mike@thesandmans.com>
2016-11-15 15:55:28 -0500

    create a run book with some default options
    - see issue #1

--- src/create-runbook.sh
+++ src/create-runbook.sh
@@ -5,6 +5,31 @@ source @BASHLIBS@/common
 
 # setup script options
 source @BASHLIBS@/shflags
-DEFINE_string name 'MY_WALK_BOOK' 'Name for the runbook' 'n'
+DEFINE_string directory 'runbook' 'Directory in which to build the runbook' 'd'
+DEFINE_string author "$(git config user.name)" 'Author of the runbook' 'a'
+DEFINE_string name "$(basename $(git config remote.origin.url))" 'Name for the runbook' 'n'
 FLAGS "$@" || log ERROR 'Failed to parse flags'
 eval set -- "${FLAGS_ARGV}"
+
+[ ${FLAGS_help} -eq ${FLAGS_TRUE} ] && exit 0
+
+SPHINX=$(which sphinx-quickstart) ||
+    {
+        log ERROR "Could not find sphinx-quickstart. Is it installed?"
+        exit 1
+    }
+
+sphinx-quickstart -q \
+    --sep \
+    --dot=_ \
+    --project="$FLAGS_name" \
+    --author="$FLAGS_author" \
+    -v 0.1 \
+    --release=0.1 \
+    --language=en \
+    --suffix=.rst \
+    --master=index\
+    --ext-todo \
+    --makefile \
+    --no-batch \
+    "$FLAGS_directory"
