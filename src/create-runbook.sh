01becbbb60b6dfdeafe2df41696af66fa6afb7cf
n8behavior <mike@thesandmans.com>
2016-11-15 15:57:12 -0500

    add contact stub to runbook (see #5)

--- src/create-runbook.sh
+++ src/create-runbook.sh
@@ -45,12 +45,25 @@ sphinx-quickstart -q \
     exit 1
 }
 
+# add livehtml target to build
 cat << EOM >> $FLAGS_directory/Makefile
 
 .PHONY: livehtml
 livehtml:
 	sphinx-autobuild -i '*.swp' -i '*.swx' -b html \$(ALLSPHINXOPTS) \$(BUILDDIR)/html
 EOM
+
+# create a contacts page in the runbook
+cat << EOM >> $FLAGS_directory/source/contacts.rst
++----------+------------+--------------+------------------+
+| Name     | Title      | Phone        | Email            |
++==========+============+==============+==================+
+| John Doe | Consultant | 123-456-7890 | john@example.com |
++----------+------------+--------------+------------------+
+EOM
+# add contacts page to index with one line of space above
+sed -i '/:maxdepth:/a \ \ \ contacts' "$FLAGS_directory/source/index.rst"
+sed -i '/:maxdepth:/G' "$FLAGS_directory/source/index.rst"
 
 SIDEBAR_CONF="html_sidebars = { '**': \
     ['globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'], }"
